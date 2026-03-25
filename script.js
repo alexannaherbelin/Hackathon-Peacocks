43962d4d4e843b345effe35fca815fdbf935f25c
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2026-03-25 18:56:51 +0000

    gobblin

--- script.js
+++ script.js
@@ -1,81 +1,3 @@
-class Tile {
-    constructor(gridX, gridY, isTile) {
-        this.gridX = gridX;
-        this.gridY = gridY;
-        this.isTile = isTile;
-        this.x = gridX * tilesize;
-        this.y = gridY * tilesize;
-        this.size = tilesize;
-    }
-
-    display() {
-        if (this.isTile === 1) {
-            fill(255);
-            rect(this.x, this.y, this.size, this.size);
-        }
-    }
-}
-
-class Player {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.size = tilesize * 0.8;
-
-        this.velocity = createVector(0, 0);
-        this.gravity = createVector(0, 0.2);
-    }
-
-    display() {
-        fill(0, 255, 0);
-        rect(this.x, this.y, this.size, this.size);
-    }
-
-    applyGravity() {
-        this.velocity.add(this.gravity);
-        this.x += this.velocity.x;
-        this.y += this.velocity.y;
-    }
-
-    collide(tiles) {
-        this.onGround = false;
-
-        for (let tile of tiles) {
-            if (tile.isTile === 1) {
-
-                if (
-                    this.x < tile.x + tile.size &&
-                    this.x + this.size > tile.x &&
-                    this.y < tile.y + tile.size &&
-                    this.y + this.size > tile.y
-                ) {
-                    
-                    if (this.velocity.y > 0 && this.y + this.size - this.velocity.y <= tile.y) {
-                        this.y = tile.y - this.size;
-                        this.velocity.y = 0;
-                        this.onGround = true;
-                    }
-
-                    else if (this.velocity.y < 0 && this.y - this.velocity.y >= tile.y + tile.size) {
-                        this.y = tile.y + tile.size;
-                        this.velocity.y = 0;
-                    }
-
-                    else if (this.velocity.x > 0) {
-                        this.x = tile.x - this.size;
-                        this.velocity.x = 0;
-                    }
-
-                    else if (this.velocity.x < 0) {
-                        this.x = tile.x + tile.size;
-                        this.velocity.x = 0;
-                    }
-                }
-            }
-        }
-    }
-}
-
 let level_1 = [
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -107,14 +29,27 @@ let level_1 = [
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
 
+let springArr = [];
+let crateArr = [];
 let tilesArr = [];
 let tilesize = 35;
-let player;
-let current_level;
+
+class Crate {
+    constructor(x, y, xSize, ySize) {
+        this.x = x;
+        this.y = y;
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+
+    display() {
+        fill(0, 0, 255); //Blue fill for objects
+        rect(this.x, this.y, this.xSize * tilesize, this.ySize * tilesize);
+    }
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    player = new Player();
     current_level = level_1;
 
     for (let y = 0; y < current_level.length; y++) {
@@ -122,7 +57,65 @@ function setup() {
             tilesArr.push(new Tile(x, y, current_level[y][x]));
         }
     }
+
+    let crate1 = new Crate(560, 175, 2, 2);
+    crateArr.push(crate1);
+
+    let spring1 = new Spring(350, 175 + 2*tilesize);
+    springArr.push(spring1);
 }
+
+class Tile {
+    constructor(gridX, gridY, isTile) {
+        this.gridX = gridX;
+        this.gridY = gridY;
+        this.isTile = isTile;
+        this.x = gridX * tilesize;
+        this.y = gridY * tilesize;
+        this.size = tilesize;
+    }
+
+    display() {
+        if (this.isTile === 1) {
+            fill(255);
+            rect(this.x, this.y, this.size, this.size);
+        }
+    }
+}
+
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.size = tilesize * 0.8;
+    }
+
+    display() {
+        fill(0, 255, 0);
+        rect(this.x, this.y, this.size, this.size);
+    }
+    gravity() {
+        this.y -= 5;
+    }
+}
+
+class Spring {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = tilesize * 0.25;
+    }
+
+    display() {
+        fill(255, 0, 255);
+        rect(this.x, this.y, 4 * this.size, this.size);
+    }
+
+    spring(){
+
+    }
+}
+
 
 function draw() {
     background(0);
@@ -131,17 +124,15 @@ function draw() {
         tile.display();
     }
 
-    if (keyIsDown(UP_ARROW) === true && player.onGround === true) {
-        player.velocity.y -= 8;
-    }
-    if (keyIsDown(LEFT_ARROW) === true) {
-        player.x -= 5;
-    }
-    if (keyIsDown(RIGHT_ARROW) === true) {
-        player.x += 5;
+    if(crateArr.length > 0) {
+        for(let i = 0; i < crateArr.length; i++) {
+            crateArr[i].display();
+        }
     }
 
-    player.display();
-    player.collide(tilesArr);
-    player.applyGravity();
+    if(springArr.length > 0) {
+        for(let i = 0; i < springArr.length; i++) {
+            springArr[i].display();
+        }
+    }
 }
